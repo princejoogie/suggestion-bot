d373e44de01a439c03f88c6f3704478cf7ca93df
princejoogie <princejoogie@gmail.com>
2021-06-13 00:20:45 +0800

    remove dist folder

--- dist/constants.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.BASE_DIR = void 0;
-exports.BASE_DIR = __dirname;
-//# sourceMappingURL=constants.js.map
--- dist/src/commands/help.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-module.exports = (msg) => {
-    let reply = "```\n";
-    reply += "!suggest <your suggestion here>";
-    reply += "```";
-    msg.channel.send(reply);
-};
-//# sourceMappingURL=help.js.map
