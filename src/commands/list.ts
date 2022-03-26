9e7fedd2ee9fcf38a2e5eeae34f2adb09a3e25f2
Josh Daniel <joshxfi.dev@gmail.com>
2022-03-26 11:08:30 +0800

    feat: add list resolved for suggestions

--- src/commands/list.ts
+++ src/commands/list.ts
@@ -1,5 +1,7 @@
 import Discord, { MessageEmbed } from "discord.js";
 import { getSuggestions } from "../helpers";
+import { Suggestion } from "../bot-types";
+import { db } from "../utils/firebase";
 
 const listSuggestions = async (msg: Discord.Message) => {
   try {
@@ -22,6 +24,43 @@ const listSuggestions = async (msg: Discord.Message) => {
     msg.channel.send(embed);
   } catch (err) {
     console.log(err);
+    msg.channel.send("`Something went wrong :(`");
+  }
+};
+
+const listResolved = async (msg: Discord.Message) => {
+  try {
+    const resolvedSuggestions = await db
+      .collection("channels")
+      .doc(msg.channel.id)
+      .collection("resolved")
+      .orderBy("timestamp", "desc")
+      .get();
+
+    const resolved = resolvedSuggestions.docs.map(
+      (doc) => ({ id: doc.id, ...doc.data() } as Suggestion)
+    );
+
+    const embed = new MessageEmbed()
+      .setColor("#0099ff")
+      .setTitle("Resolved Suggestions");
+
+    if (resolved.length > 0) {
+      resolved.map((e, i) => {
+        embed.addField(
+          `${i + 1}. ${e.suggestion} (${e.votes.length} vote/s)`,
+          `Suggested by: ${e.user.name}`
+        );
+      });
+    } else {
+      embed.addField(
+        "No Resolved Suggestions yet.",
+        "use `>resolve` to add one"
+      );
+    }
+
+    msg.channel.send(embed);
+  } catch (err) {
     msg.channel.send("`Something went wrong :(`");
   }
 };
@@ -55,6 +94,8 @@ module.exports = (msg: Discord.Message, args: string[]) => {
     listSuggestions(msg);
   } else if (args.join(" ").trim() === "mine") {
     listSuggestionsByID(msg, msg.author.id);
+  } else if (args.join(" ").trim() === "resolved") {
+    listResolved(msg);
   } else {
     msg.reply("Not a command.");
   }
